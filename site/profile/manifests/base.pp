94cde5670f5eab55bc252ddbe78635dc5fa74bfb
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-23 11:23:30 +0200

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -7,9 +7,4 @@ class profile::base {
       ensure => file,
       content => "Ceci est un readme\nMon adresse ip est ${ipaddress}\nMon nom est:\n${fqdn}\n",
   }
-  
-  file { '/etc/motd':
-      ensure => file,
-      content => "Bonjour, vous êtes sur la machine ${fqdn}, ip ${ipaddress}\n",
-  }
 }
