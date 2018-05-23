461d54d4bf67a661554c3c952ed1f2c91c71689e
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-23 12:33:54 +0200

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -8,13 +8,17 @@ class profile::base {
     content => "Ceci est un readme\nMon adresse ip est ${ipaddress}\nMon nom est:\n${fqdn}\n",
   }
   
-  file_line { 'Append a first client to /etc/hosts':
-    path => '/etc/hosts',
-    line => '10.94.69.4  puppet-client-1',
+  host { 'puppet-client-1':
+    name         => 'puppet-client-1,
+    ensure       => file,
+    host_aliases => 'firstclient'
+    ip           => '10.94.69.4'
   }
   
-  file_line { 'Append a second client to /etc/hosts':
-    path => '/etc/hosts',
-    line => '10.94.69.8  puppet-client-2',
+  host { 'puppet-client-2':
+    name         => 'puppet-client-2,
+    ensure       => file,
+    host_aliases => 'secondclient'
+    ip           => '10.94.69.8'
   }
 }
