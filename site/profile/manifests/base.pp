60dc6710593b29882bc472642315b15b68b0cc63
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-23 12:02:05 +0200

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,7 +4,17 @@ class profile::base {
   }
 
   file { '/root/README':
-      ensure => file,
-      content => "Ceci est un readme\nMon adresse ip est ${ipaddress}\nMon nom est:\n${fqdn}\n",
+    ensure => file,
+    content => "Ceci est un readme\nMon adresse ip est ${ipaddress}\nMon nom est:\n${fqdn}\n",
+  }
+  
+  file_line { 'Append a first client to /etc/hosts':
+    path => '/etc/hosts',
+    line => '10.94.69.4  puppet-client-1',
+  }
+  
+  file_line { 'Append a second client to /etc/hosts':
+    path => '/etc/hosts',
+    line => '10.94.69.8  puppet-client-2',
   }
 }
