92087ebc63664604e2b141f4f8dea5e83ea21ae7
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-24 15:42:28 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,14 +1,16 @@
-class minecraft {
+class minecraft (
+ $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+ $install_dir = '/opt/minecraft'){
   include ::wget
   
-  file { '/opt/minecraft':
+  file { "${install_dir}":
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode    => '0644',
   }
   
-  file { '/opt/minecraft/eula.txt':
+  file { "${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
     owner  => 'root',
@@ -18,7 +20,11 @@ class minecraft {
   
   file { '/etc/systemd/system/minecraft.service':
       ensure => file,
-      content => "[Unit]\nDescription=Minecraft server\nAfter=network-online.target\nWants=network.target\nRequires=network-online.target\nConditionPathExists=/opt/minecraft\n[Service]\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\nRestart=always\nRestartPreventExitStatus=255\nType=notify\n[Install]\nWantedBy=multi-user.target\nAlias=minecraft\n",
+      #content => "[Unit]\nDescription=Minecraft server\nAfter=network-online.target\nWants=network.target\nRequires=network-online.target\nConditionPathExists=/opt/minecraft\n[Service]\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\nRestart=always\nRestartPreventExitStatus=255\nType=notify\n[Install]\nWantedBy=multi-user.target\nAlias=minecraft\n",
+      # source  =>  'puppet:///modules/minecraft/minecraft.service', Mettre minecraft.service dans control_repo/site/minecraft/files/
+      content  => epp('minecraft/minecraft.service.epp', {
+           install_dir => $install_dir
+      }),
       owner  => 'root',
       group  => 'root',
       mode    => '0644',
@@ -28,9 +34,10 @@ class minecraft {
     ensure  =>  present,
   }
 
-  wget::fetch { 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar':
-    destination => '/opt/minecraft/',
+  wget::fetch {"${url}":
     timeout     => 15,
+    destination => "${install_dir}/",
+    before      => Service['minecraft'],
     verbose     => true,
   }
   
@@ -39,7 +46,7 @@ class minecraft {
     enable  =>  true,
     require => [
       Package['default-jre'],
-      File['/opt/minecraft/eula.txt'],
+      File["${install_dir}/eula.txt"],
       File['/etc/systemd/system/minecraft.service']
       ],
   }
