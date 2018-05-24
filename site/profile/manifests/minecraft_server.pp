e6f987066812a610404588fd54e678b9735ed474
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-24 11:10:09 +0200

    Create minecraft_server.pp

---
+++ site/profile/manifests/minecraft_server.pp
@@ -0,0 +1,10 @@
+class profile::minecraft_server {
+  include ::wget
+
+  wget::fetch { 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar':
+    destination => '/opt/minecraft/',
+    timeout     => 15,
+    verbose     => true,
+  }
+
+}
