fc630a2cc1bc5ebc076421b076ba87f64eedc19b
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-24 14:22:52 +0200

    Update minecraft_server.pp

--- site/profile/manifests/minecraft_server.pp
+++ site/profile/manifests/minecraft_server.pp
@@ -23,6 +23,10 @@ class profile::minecraft_server {
       group  => 'root',
       mode    => '0644',
   }
+  
+  package {'default-jre':
+    ensure  =>  present,
+  }
 
   wget::fetch { 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar':
     destination => '/opt/minecraft/',
