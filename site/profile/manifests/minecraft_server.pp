2fec0762b1ac6e8b5d3997ea9d6197d353427370
LaurentMonribot <37579921+LaurentMonribot@users.noreply.github.com>
2018-05-24 11:20:16 +0200

    Update minecraft_server.pp

--- site/profile/manifests/minecraft_server.pp
+++ site/profile/manifests/minecraft_server.pp
@@ -8,6 +8,14 @@ class profile::minecraft_server {
     mode    => '0644',
   }
   
+  file { '/opt/minecraft/minecraft.service':
+      ensure => file,
+      content => "[Unit]\nDescription=Minecraft server\nAfter=network-online.target\nWants=network.target\nRequires=network-online.target\nConditionPathExists=/opt/minecraft\n[Service]\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx512M -Xms512M -jar server.jar nogui\nExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=always\nRestartPreventExitStatus=255\nType=notify\n[Install]\nWantedBy=multi-user.target\nAlias=minecraft\n",
+      owner  => 'root',
+      group  => 'root',
+      mode    => '0644',
+  }
+
   wget::fetch { 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar':
     destination => '/opt/minecraft/',
     timeout     => 15,
